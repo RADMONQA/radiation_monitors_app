c9728c6c474cf78ca4acac491fd968b8f424a86f
Szymon Bednorz <dsonyyy@gmail.com>
2024-10-09 18:25:54 +0200

    move ansible files

---
+++ ansible/files/radem-notebooks/processing/0_fetching_ftp.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+
+# Build required folder structure
+mkdir -p ../data
+mkdir -p ../data/raw
+mkdir -p ../data/extracted
+mkdir -p ../data/csv
+
+DATA_RAW_DIR="../data/raw"
+
+# Load environment variables
+export $(grep -v '^#' ../.env | xargs)
+
+# Fetch data from ftp
+wget -r \
+    --timestamping \
+    --continue \
+    --user=${FTP_USER} \
+    --password=${FTP_PASSWORD} \
+    ftp://ftptrans.psi.ch/to_radem/ \
+    -nd \
+    -np \
+    -P ${DATA_RAW_DIR}\
+    
