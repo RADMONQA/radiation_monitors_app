e2d55530a612415b350e72895656c6b9fdb27c09
Szymon Bednorz <dsonyyy@gmail.com>
2024-11-25 14:22:47 +0100

    deploy: cyfronet config

--- app/scripts/fetching/fetch_all.sh
+++ app/scripts/fetching/fetch_all.sh
@@ -4,9 +4,9 @@
 SCRIPTS_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 # Data fetching
-echo "============= FETCHING RADEM DATA ============="
-${SCRIPTS_DIR}/fetch_radem.sh
-echo -e "\e[32mRADEM DATA FETCHED SUCCESSFULLY\e[0m"
+# echo "============= FETCHING RADEM DATA ============="
+# ${SCRIPTS_DIR}/fetch_radem.sh
+# echo -e "\e[32mRADEM DATA FETCHED SUCCESSFULLY\e[0m"
 
 echo "============= FETCHING IREM DATA ============="
 ${SCRIPTS_DIR}/fetch_irem.sh
