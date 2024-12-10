1a126787c98f43ead583282d200feecd24c4c476
Szymon Bednorz <dsonyyy@gmail.com>
2024-12-10 03:20:51 +0100

    fix: remove unnecassary timeout

--- app/scripts/fetching/fetch_all.sh
+++ app/scripts/fetching/fetch_all.sh
@@ -3,16 +3,13 @@
 # Get the directory of the script
 SCRIPTS_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-# Timeout 
-TIMEOUT=30m
-
 # Data fetching
-echo "============= FETCHING RADEM DATA =============" 
-timeout ${TIMEOUT} ${SCRIPTS_DIR}/fetch_radem.sh
+echo "============= FETCHING RADEM DATA ============="
+${SCRIPTS_DIR}/fetch_radem.sh
 echo -e "\e[32mRADEM DATA FETCHED SUCCESSFULLY\e[0m"
 
-echo "============= FETCHING IREM DATA =============" 
-timeout ${TIMEOUT} ${SCRIPTS_DIR}/fetch_irem.sh
+echo "============= FETCHING IREM DATA ============="
+${SCRIPTS_DIR}/fetch_irem.sh
 echo -e "\e[32mIREM DATA FETCHED SUCCESSFULLY\e[0m"
 
 # Print "ok" in green
