43d7fdff5307ceb8901e8afcdae6deaa3e9cd40a
Elemental-afk <domikk2003@gmail.com>
2025-09-11 15:50:03 +0200

    initial fix

--- app/scripts/fetching/fetch_radem.sh
+++ app/scripts/fetching/fetch_radem.sh
@@ -8,13 +8,15 @@ mkdir -p ${DATA_DIR}/radem/raw
 mkdir -p ${DATA_DIR}/radem/extracted
 mkdir -p ${DATA_DIR}/radem/csv
 
-# Fetch data from ftp
-wget -r \
-    --timestamping \
-    --continue \
-    --user=${FTP_USER} \
-    --password=${FTP_PASSWORD} \
-    ${FTP_URL} \
-    -nd \
-    -np \
-    -P ${DATA_DIR}/radem/raw\
+# TODO - replace with current HTTP link - FTP is deprecated
+
+# # Fetch data from ftp
+# wget -r \
+#     --timestamping \
+#     --continue \
+#     --user=${FTP_USER} \
+#     --password=${FTP_PASSWORD} \
+#     ${FTP_URL} \
+#     -nd \
+#     -np \
+#     -P ${DATA_DIR}/radem/raw\
