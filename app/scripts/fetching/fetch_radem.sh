5089179c90ff4e6aca5b5b49078c05012c952ff9
Elemental-afk <domikk2003@gmail.com>
2025-09-24 14:59:42 +0200

    path fixes

--- app/scripts/fetching/fetch_radem.sh
+++ app/scripts/fetching/fetch_radem.sh
@@ -5,12 +5,12 @@
 # Build required folder structure
 
 # Contains all downloaded cdfs permanently
-mkdir -p /app_data/radem/archive
+mkdir -p {$DATA_DIR}/radem/archive
 
 # Temporary directiories for downloading and processing
-mkdir -p /app_data/radem/extracted/hk
-mkdir -p /app_data/radem/extracted/sc
-mkdir -p /app_data/radem/csv
+mkdir -p {$DATA_DIR}/radem/extracted/hk
+mkdir -p {$DATA_DIR}/radem/extracted/sc
+mkdir -p {$DATA_DIR}/radem/csv
 
 # Download the raw data
 
@@ -25,7 +25,7 @@ wget \
     --cut-dirs=5 \
     -A cdf \
     https://archives.esac.esa.int/psa/repo/ftp-public/Juice/juice_radem/data_raw/ \
-    -P /app_data/radem/archive \
+    -P DATA_DIR/radem/archive \
     -o logfile \
     --output-file=wget.log \
     -nv
@@ -34,29 +34,29 @@ echo "RADEM data fetch completed"
 
 # Cleanup the log file
 
-grep -o '"[^"]*\.cdf"' wget.log | sed 's/"//g' > ../wget_cleaned.log
+grep -o '"[^"]*\.cdf"' wget.log | sed 's/"//g' > {$DATA_DIR}/radem/logs/wget_cleaned.log
 
 rm wget.log
 
 #Get line count of cleaned log
-LINECOUNT=$(wc -l < ../wget_cleaned.log)
+LINECOUNT=$(wc -l < {$DATA_DIR}/radem/logs/wget_cleaned.log)
 echo "Number of files downloaded: $LINECOUNT"
 echo "Copying files to processing directory"
 
 # Get all downloaded files and copy them to the processing directory
 
 # For HK files
-grep "radem_raw_hk" ../wget_cleaned.log | xargs -I filepath \
-  cp "filepath" "/app_data/radem/extracted/hk/"
+grep "radem_raw_hk" {$DATA_DIR}/radem/logs/wget_cleaned.log | xargs -I filepath \
+  cp {} {$DATA_DIR}/radem/extracted/hk/
 
-# For SC files  
-grep "radem_raw_sc" ../wget_cleaned.log | xargs -I filepath \
-  cp "filepath" "/app_data/radem/extracted/sc/"
+# For SC files
+grep "radem_raw_sc" {$DATA_DIR}/radem/logs/wget_cleaned.log | xargs -I filepath \
+  cp {} {$DATA_DIR}/radem/extracted/sc/
 
 echo "Files copied to processing directory"
 
-HKCOUNT=$(ls /app_data/radem/extracted/hk/ | wc -l)
-SCCOUNT=$(ls /app_data/radem/extracted/sc/ | wc -l)
+HKCOUNT=$(ls {$DATA_DIR}/radem/extracted/hk/ | wc -l)
+SCCOUNT=$(ls {$DATA_DIR}/radem/extracted/sc/ | wc -l)
 
 echo "Number of HK files to process: $HKCOUNT"
 echo "Number of SC files to process: $SCCOUNT"
