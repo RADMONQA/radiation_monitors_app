2e99dde99da6ea2caf476c38db4b49886840fc78
Elemental-afk <domikk2003@gmail.com>
2025-09-24 15:28:31 +0200

    fix

--- app/scripts/publishing/4_log_and_cleanup.sh
+++ app/scripts/publishing/4_log_and_cleanup.sh
@@ -6,17 +6,17 @@
 echo "Starting log and cleanup"
 
 # Create the log file with header if it doesn't exist
-mkdir -p {$DATA_DIR}/radem/logs && touch {$DATA_DIR}/radem/logs/download_log.csv
+mkdir -p ${DATA_DIR}/radem/logs && touch ${DATA_DIR}/radem/logs/download_log.csv
 
-if [ ! -s {$DATA_DIR}/radem/logs/download_log.csv ]; then
-    echo "id,timestamp,path,sha256_checksum" >> {$DATA_DIR}/radem/logs/download_log.csv
+if [ ! -s ${DATA_DIR}/radem/logs/download_log.csv ]; then
+    echo "id,timestamp,path,sha256_checksum" >> ${DATA_DIR}/radem/logs/download_log.csv
 fi
 
 DATETIME=$(date -u +"%Y-%m-%d %T")
 
 # Determine the next ID taking into account an empty or non-existent log file
-if [ -s {$DATA_DIR}/radem/logs/download_log.csv ]; then
-    LAST_ID=$(tail -n 1 {$DATA_DIR}/radem/logs/download_log.csv | cut -d',' -f1)
+if [ -s ${DATA_DIR}/radem/logs/download_log.csv ]; then
+    LAST_ID=$(tail -n 1 ${DATA_DIR}/radem/logs/download_log.csv | cut -d',' -f1)
     if echo "$LAST_ID" | grep -qE '^[0-9]+$'; then
         CURRENT_ID=$((LAST_ID + 1))
     else
@@ -30,18 +30,18 @@ fi
 while IFS= read -r line; do
     FILEPATH="$line"
     CHECKSUM=$(sha256sum "$FILEPATH" | awk '{print $1}')
-    echo "$CURRENT_ID,$DATETIME,$FILEPATH,$CHECKSUM" >> {$DATA_DIR}/radem/logs/download_log.csv
+    echo "$CURRENT_ID,$DATETIME,$FILEPATH,$CHECKSUM" >> ${DATA_DIR}/radem/logs/download_log.csv
     echo "Logged file with ID $CURRENT_ID: $FILEPATH with checksum $CHECKSUM"
     CURRENT_ID=$((CURRENT_ID + 1))
 
-done < {$DATA_DIR}/radem/logs/wget_cleaned.log
+done < ${DATA_DIR}/radem/logs/wget_cleaned.log
 
 # Cleanup temporary and intermediate files
-rm -rf {$DATA_DIR}/radem/extracted/hk/*
-rm -rf {$DATA_DIR}/radem/extracted/sc/*
-rm -rf {$DATA_DIR}/radem/csv/*
-rm -rf {$DATA_DIR}/radem/line_protocol/*
+rm -rf ${DATA_DIR}/radem/extracted/hk/*
+rm -rf ${DATA_DIR}/radem/extracted/sc/*
+rm -rf ${DATA_DIR}/radem/csv/*
+rm -rf ${DATA_DIR}/radem/line_protocol/*
 
-rm {$DATA_DIR}/radem/logs/wget_cleaned.log
+rm ${DATA_DIR}/radem/logs/wget_cleaned.log
 
 echo "Log and cleanup completed"
